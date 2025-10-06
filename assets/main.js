9621b0366f0fd3563f3e92ceeefeff154b8bd843
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-06 13:53:24 +0200

    add like comments the tasks about the calc of cost of the travel

--- assets/main.js
+++ assets/main.js
@@ -15,4 +15,11 @@ let km = Number(prompt('Inserisci i chilometri che vuoi percorrere'))
 // Inserimento dell'età del passeggero
 let age = Number(prompt('Inserisci la tua età'))
 
+/* Calcolo del prezzo totale del viaggio */
+
+// Calcolo il prezzo in base ai chilometri
+// Applico lo sconto del 20% se il passeggero è minorenne
+// Applico lo sconto del 40% se il passeggero è over 65
+// Mostro come output il prezzo totale del viaggio
+
 
