5b57d6ac48ad22169e876c20f009d8a7a27535f2
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-06 13:43:19 +0200

    add comments in js for know struments and tasks

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+/*
+TOOLS:
+
+- promt
+- const / let
+- operatore di comparazione
+- if / else
+*/
+
+/* Inizializzazione valori con inserimento da input */
+
+// Inserimento dei chilometri da percorrere
+// Inserimento dell'età del passeggero
+
