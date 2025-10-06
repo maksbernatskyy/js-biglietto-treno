5819f542f7537a7d43524420a27d3be9aba123c4
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-06 13:58:18 +0200

    add calc of baseCost

--- assets/main.js
+++ assets/main.js
@@ -15,9 +15,14 @@ let km = Number(prompt('Inserisci i chilometri che vuoi percorrere'))
 // Inserimento dell'età del passeggero
 let age = Number(prompt('Inserisci la tua età'))
 
+// Prezzo al chilometro
+const costAtKm = 0.21
+
 /* Calcolo del prezzo totale del viaggio */
 
 // Calcolo il prezzo in base ai chilometri
+let baseCost = costAtKm * km
+console.log(baseCost)
 // Applico lo sconto del 20% se il passeggero è minorenne
 // Applico lo sconto del 40% se il passeggero è over 65
 // Mostro come output il prezzo totale del viaggio
